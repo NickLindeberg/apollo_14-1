c39f5e75267fc3f2199e09b9652077b6a6500a68
Nick Lindeberg <lindebergnick@gmail.com>
2018-10-12 10:30:48 -0600

    it 2 down

--- spec/features/user_sees_astronaut_index_spec.rb
+++ spec/features/user_sees_astronaut_index_spec.rb
@@ -11,4 +11,13 @@ describe 'user visits astronaut index' do
     expect(page).to have_content(astronaut.job)
 
   end
+
+  it 'shows avg age for astronauts' do
+    astronaut = Astronaut.create(name: "Nick Lindy", age: 40, job: "Commander")
+    astronaut = Astronaut.create(name: "Rick Nindy", age: 20, job: "Chef")
+
+    visit astronauts_path
+
+    expect(page).to have_content("Average Age: 30")
+  end
 end
--- spec/models/astronaut_spec.rb
+++ spec/models/astronaut_spec.rb
@@ -11,4 +11,14 @@ describe Astronaut, type: :model do
     it { should have_many :astronaut_missions}
     it { should have_many :missions}
   end
+
+  describe 'class methods' do
+    it 'should show avg age for all astronauts' do
+      astronaut = Astronaut.create(name: "Nick Lindy", age: 40, job: "Commander")
+      astronaut = Astronaut.create(name: "Rick Nindy", age: 20, job: "Chef")
+      avgerage_age = Astronaut.avg_age
+
+      expect(avgerage_age).to eq(30)
+    end
+  end
 end
